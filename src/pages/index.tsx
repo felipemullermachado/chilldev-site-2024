4d0f6d223dc76b869663ca24c9c650d749841bc6
Felipe Muller Machado <felipe.machado@dotgroup.com.br>
2024-01-03 19:01:41 -0300

    template hero page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
+import imgFace from '../assets/img/img-face.jpg'
+import cn from 'classnames'
+import st from '../assets/styles/pageHome.module.styl'
+
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className={st.core}>
+      <section className={cn('container', st.hero)}>
+        <h1>
+          Does your team needs a{' '}
+          <span className="block">
+            <span className="color-purple">Frontend Developer</span> ?
+          </span>
+        </h1>
+        <p className="large-size">
+          I convert user interface prototypes into code, implementing a
+          responsive and user-friendly website.
+        </p>
+        <p className="large-size">
+          I work closely with UX/UI designers to deliver the best user
+          experience.
+        </p>
+        <div className={st.containerImg}>
+          <img src={imgFace.src} alt="" />
+        </div>
+      </section>
     </main>
   )
 }
