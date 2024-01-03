14902b3b11461fe45fc2e70e93d727a3809ebf20
Felipe Muller Machado <felipe.machado@dotgroup.com.br>
2024-01-03 17:42:56 -0300

    add fonts file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import 'reset-css'
-// import '../styles/global.styl'
+import '../assets/styles/global.styl'
 import type { AppProps } from 'next/app'
  
 export default function App({ Component, pageProps }: AppProps) {
