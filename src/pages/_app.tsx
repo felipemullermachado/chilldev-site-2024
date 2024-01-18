a65dbbcab120ce0ae713f8b94c2644cb13bc3b36
Felipe Muller Machado <felipe.machado@dotgroup.com.br>
2024-01-18 16:18:21 -0300

    fix indent

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import 'reset-css'
 import '../assets/styles/global.styl'
 import type { AppProps } from 'next/app'
- 
-export default function App({ Component, pageProps, router: { asPath } }: AppProps) {
+
+export default function App({
+  Component,
+  pageProps,
+  router: { asPath },
+}: AppProps) {
   const pathname = asPath.split('?')[0]
   return <Component pathname={pathname} {...pageProps} />
 }
