f2f335769d9ce22951f14b8154d5621ec33fa355
Felipe Muller Machado <felipe.machado@dotgroup.com.br>
2024-01-18 19:15:24 -0300

    implement Slider in mobile

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,26 +1,52 @@
 import Image, { StaticImageData } from 'next/image'
+import { useBreakpoints } from '@/hooks'
+import Slider from '@fil1pe/react-slider'
 import st from '../assets/styles/Cards.module.styl'
 
 export function Cards({ cards }: CardsProps) {
+  const breakpoint = useBreakpoints()
   return (
-    <ul className={st.core}>
-      {cards.map(({ title, company, imgSrc, link }, index) => (
-        <li key={index}>
-          <section>
-            <h3 className="title little-size">{title}</h3>
-            <Image src={imgSrc} alt={`${company} company illustration`} />
-            <a
-              href={link}
-              title={`Visit ${company}'s website`}
-              target="_blank"
-              className="btn"
-            >
-              Visit Website
-            </a>
-          </section>
-        </li>
-      ))}
-    </ul>
+    <>
+      {breakpoint !== 'phone' ? (
+        <ul className={st.core}>
+          {cards.map(({ title, company, imgSrc, link }, index) => (
+            <li key={index} className={st.card}>
+              <section>
+                <h3 className="title little-size">{title}</h3>
+                <Image src={imgSrc} alt={`${company} company illustration`} />
+                <a
+                  href={link}
+                  title={`Visit ${company}'s website`}
+                  target="_blank"
+                  className="btn"
+                >
+                  Visit Website
+                </a>
+              </section>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className={st.core}>
+          <Slider slidesToShow={1}>
+            {cards.map(({ title, company, imgSrc, link }, index) => (
+              <section key={index} className={st.card}>
+                <h3 className="title little-size">{title}</h3>
+                <Image src={imgSrc} alt={`${company} company illustration`} />
+                <a
+                  href={link}
+                  title={`Visit ${company}'s website`}
+                  target="_blank"
+                  className="btn"
+                >
+                  Visit Website
+                </a>
+              </section>
+            ))}
+          </Slider>
+        </div>
+      )}
+    </>
   )
 }
 
