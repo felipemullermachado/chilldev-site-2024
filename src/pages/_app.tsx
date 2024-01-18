cf03f7760954da514f0198705f634adf0977026f
Felipe Muller Machado <felipe.machado@dotgroup.com.br>
2024-01-18 16:18:00 -0300

    fix pathname

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import 'reset-css'
 import '../assets/styles/global.styl'
 import type { AppProps } from 'next/app'
  
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router: { asPath } }: AppProps) {
+  const pathname = asPath.split('?')[0]
+  return <Component pathname={pathname} {...pageProps} />
 }
